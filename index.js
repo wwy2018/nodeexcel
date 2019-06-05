f62edb04a119439ca4b2148dc5c68326a3b44b54
wwy2018 <554135393@qq.com>
2019-06-05 17:16:35 +0800

    .

---
+++ index.js
@@ -0,0 +1,36 @@
+const xlsx=require('xlsx')
+const fs=require('fs')
+let f=xlsx.readFile('diancan.xlsx')
+let keys = ['Menu', 'Order', 'Placeorder', 'Orderdetail', 'Noneali']
+let en={}
+let ch={}
+let menukeys=['scan','menu','order','sold','choose','reselect','options','price','total','checkout','empty']
+let orderkeys=['eatin','pickno','buytime','paysuc','refund','noorder','refresh','myorder','nomore','unit']
+let placeorderkeys=['tableno','optional','comments','addcomment','total','payali','confirm','paying','alifail','reload','error']
+let orderdetailkeys=['paysuc','pickno','close','view','total','comments','orderno','buytime','payway','payali']
+let nonealikeys=['welcome','scan','aliapp']
+let subkeys={
+  menukeys: menukeys,
+  orderkeys: orderkeys,
+  placeorderkeys: placeorderkeys,
+  orderdetailkeys: orderdetailkeys,
+  nonealikeys: nonealikeys
+}
+f.SheetNames.map((item, index) => {
+  let shet = f.Sheets[item]
+  let sk = Object.values(subkeys)[index]
+  if (!en[keys[index]]) en[keys[index]]={}
+  if (!ch[keys[index]]) ch[keys[index]]={}
+  let idx = 0
+  for (let i in shet) {
+    let x = parseInt(idx)
+    if (i.startsWith('A')) {
+      ch[keys[index]][sk[x]] = shet[i]['v']
+    } else if (i.startsWith('B')) {
+      en[keys[index]][sk[x]] = shet[i]['v']
+    }
+    idx++
+  }
+})
+fs.writeFile('en.json', JSON.stringify(en), {flag: 'w+'}, ()=>{})
+fs.writeFile('ch.json', JSON.stringify(ch), {flag: 'w+'}, ()=>{})
